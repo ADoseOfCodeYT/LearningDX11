98d7afb0144672e2c0158cf041922ecb440160f9
ADoseOfCode <140978248+ADoseOfCodeYT@users.noreply.github.com>
2025-03-08 16:20:43 +0000

    specular highlights

--- LearningDX11/Core/Application/ApplicationClass.cpp
+++ LearningDX11/Core/Application/ApplicationClass.cpp
@@ -37,10 +37,10 @@ bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
     m_Camera = new CameraClass;
 
     // Set the initial position of the camera.
-    m_Camera->SetPosition(0.0f, 0.0f, -10.0f);
+    m_Camera->SetPosition(0.0f, 0.0f, -5.0f);
 
     // Set the file name of the model.
-    strcpy_s(modelFilename, "Content/cube.txt");
+    strcpy_s(modelFilename, "Content/Sphere.txt");
 
     // Set the name of the texture file that we will be loading.
     strcpy_s(textureFilename, "Content/textures/stone01.tga");
@@ -70,7 +70,9 @@ bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 
     m_Light->SetAmbientColor(0.15f, 0.15f, 0.15f, 1.0f);
     m_Light->SetDiffuseColor(1.0f, 1.0f, 1.0f, 1.0f);
-    m_Light->SetDirection(1.0f, 0.0f, 0.0f);
+    m_Light->SetDirection(1.0f, 0.0f, 1.0f);
+    m_Light->SetSpecularColor(1.0f, 1.0f, 1.0f, 1.0f);
+    m_Light->SetSpecularPower(32.0f);
     
     return true;
 }
@@ -146,7 +148,7 @@ bool ApplicationClass::Frame()
 
 bool ApplicationClass::Render(float rotation)
 {
-    XMMATRIX worldMatrix, viewMatrix, projectionMatrix, rotateMatrix, translateMatrix, scaleMatrix, srMatrix;
+    XMMATRIX worldMatrix, viewMatrix, projectionMatrix, rotateMatrix, translateMatrix;
     bool result;
 
 
@@ -171,8 +173,10 @@ bool ApplicationClass::Render(float rotation)
     m_Model->Render(m_Direct3D->GetDeviceContext());
 
     // Render the model using the light shader.
+    // Render the model using the light shader.
     result = m_LightShader->Render(m_Direct3D->GetDeviceContext(), m_Model->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, m_Model->GetTexture(),
-                                   m_Light->GetDirection(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor());
+                                   m_Light->GetDirection(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor(),
+                                   m_Camera->GetPosition(), m_Light->GetSpecularColor(), m_Light->GetSpecularPower());
     if(!result)
     {
         return false;
